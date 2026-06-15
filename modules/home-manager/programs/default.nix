c450efa7f89dd2bbfb437910fbbaa869252de8df
Solavane <solavane@proton.me>
2026-06-16 01:05:25 +0200

    fixed nvim's evaluation errors, tabstop, and fixed foot font

--- modules/home-manager/programs/default.nix
+++ modules/home-manager/programs/default.nix
@@ -15,21 +15,61 @@ in {
     };
   };
 
+  home.packages = lib.mkIf p.foot.enable [
+    pkgs.nerd-fonts.jetbrains-mono
+  ];
+  programs.foot = lib.mkIf p.foot.enable {
+    enable = true;
+    server.enable = true;
+    settings = {
+      main = {
+        font = "JetBrainsMono Nerd Font:size=11";
+
+      };
+    };
+  };
+
   programs.neovim = {
     enable         = true;
     viAlias        = true;
     vimAlias       = true;
     defaultEditor  = true;
     waylandSupport = true;
+    withRuby       = false;
+    withPython3    = false;
     extraConfig = ''
+      set termguicolors
       set number
       set tabstop=2
       set softtabstop=2
+      set shiftwidth=2
       set expandtab
       set nocompatible
       set wildmode=longest,list
       set clipboard=unnamedplus 
     '';
+    plugins = [
+      {
+        plugin = pkgs.vimPlugins.nvim-tree-lua;
+	type = "lua";
+        config = ''
+          require('nvim-tree').setup()
+        '';
+      }
+      {
+        plugin = pkgs.vimPlugins.vim-startify;
+        config = "let g:startify_change_to_vcs_root = 0";
+      }
+      {
+        plugin = pkgs.vimPlugins.telescope-nvim;
+        type = "lua"; 
+        config = ''
+          require('telescope').setup()
+          
+          vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, {})
+          vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, {})
+        '';
+      }
+    ];
   };
-  # Add profiles, extensions etc. here later.
 }
